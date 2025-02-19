96663a086493446bc6d4ba87b49a5dafeddbc679
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-02-19 18:55:28 +0530

    Javascript in Progress

--- Javascript/Day 1/script.js
+++ Javascript/Day 1/script.js
@@ -1,1 +1,2 @@
-alert('I am Vansh')
+
+// console.log(typeof NaN);
---
+++ Javascript/Day 1/script2.js
@@ -0,0 +1,27 @@
+/*Age Category Message – Ask the user for their age. If they are
+under 18, print “You are a minor.” If they are between 18 and 60,
+print “You are an adult.” If they are above 60, print “You are a
+senior citizen.” */
+
+// var age = Number(prompt('Enter your Age'))
+
+// console.log('Your age is', age);
+
+// if(age < 18)
+// {
+//     console.log('You are a minor.');
+// }
+
+// else if(age >= 18 && age <=60)
+// {
+//     console.log('You are an adult.');
+// }
+
+// else
+// {
+//     console.log('You are a senior citizen.');
+// }
+
+// Que 1 completed
+
+
