09d44fa20550b4b7c0a883cecb0830bd7b560ab5
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-02-20 19:15:18 +0530

    Javascript Questions in progress

--- Javascript/Day 2/script.js
+++ Javascript/Day 2/script.js
@@ -25,22 +25,3 @@ senior citizen.” */
 // Que 1 completed
 
 
-// Even or Odd Sum – Take two numbers from the user using
-// prompt(). If the sum of both numbers is even, print “Even Sum”;
-// otherwise, print “Odd Sum.”
-
-var num1 = Number(prompt('Enter number 1'))
-var num2 = Number(prompt('Enter number 2'))
-
-var sum = num1 + num2 ;
-console.log('The sum is', sum);
-
-if(sum % 2 == 0)
-{
-    console.log('Even Sum');
-}
-
-else
-{
-    console.log('Odd Sum');
-}
