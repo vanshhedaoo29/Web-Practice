63f6165efc69ab7781f47aa3b7495a573dd36181
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-02-21 09:08:30 +0530

    Day 4 Javascript

---
+++ Javascript/Day 4/script.js
@@ -0,0 +1,62 @@
+function abcd()
+{
+    // Function 
+}
+
+function abcd(a,b,c)   // a, b , c are parameters
+{
+    console.log(a,b,c);
+}
+abcd(1,2,3);              // 1, 2, 3 are arguments
+
+
+// Function Types
+
+function abcd()
+{
+    console.log('Hello Vansh');
+    // Function statement
+}
+
+abcd = function()
+{
+    console.log('Hello Vansh');
+    // Function expression
+}
+
+function ()
+{
+    console.log('Hello Vansh');
+    // Anonymous function
+}
+
+()=>
+{
+    // Fat arrow function
+}
+
+let abcd = (a)=>
+{
+    // Fat arrow with one argument
+}
+abcd(20);
+
+function abcd()
+{
+    return 12;    // Explicit return function
+}
+abcd();
+
+let abcd = ()=> 12
+    {
+        // Fat arrow with implicit return
+    }
+ var ans = abcd();
+
+ // Rest parameter
+
+ function abcd(a,b,c,...rest)   // ... is rest
+ {
+    console.log(a,b,c,rest);
+ }
+ abcd(1,2,3,4,5,6,7,8,9);
