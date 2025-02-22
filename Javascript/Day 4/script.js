3beba69ce49c8de23c32e88325696d50ff58c5ed
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-02-22 19:55:20 +0530

    WD Practicals added

--- Javascript/Day 4/script.js
+++ Javascript/Day 4/script.js
@@ -1,62 +1,96 @@
-function abcd()
-{
-    // Function 
-}
+// // // function abcd()
+// // // {
+// // //     // Function 
+// // // }
 
-function abcd(a,b,c)   // a, b , c are parameters
-{
-    console.log(a,b,c);
-}
-abcd(1,2,3);              // 1, 2, 3 are arguments
+// // // function abcd(a,b,c)   // a, b , c are parameters
+// // // {
+// // //     console.log(a,b,c);
+// // // }
+// // // abcd(1,2,3);              // 1, 2, 3 are arguments
 
 
-// Function Types
+// // // // Function Types
 
-function abcd()
-{
-    console.log('Hello Vansh');
-    // Function statement
-}
+// // // function abcd()
+// // // {
+// // //     console.log('Hello Vansh');
+// // //     // Function statement
+// // // }
 
-abcd = function()
-{
-    console.log('Hello Vansh');
-    // Function expression
-}
+// // // abcd = function()
+// // // {
+// // //     console.log('Hello Vansh');
+// // //     // Function expression
+// // // }
 
-function ()
-{
-    console.log('Hello Vansh');
-    // Anonymous function
-}
+// // // function ()
+// // // {
+// // //     console.log('Hello Vansh');
+// // //     // Anonymous function
+// // // }
 
-()=>
-{
-    // Fat arrow function
-}
+// // // ()=>
+// // // {
+// // //     // Fat arrow function
+// // // }
 
-let abcd = (a)=>
-{
-    // Fat arrow with one argument
-}
-abcd(20);
+// // // let abcd = (a)=>
+// // // {
+// // //     // Fat arrow with one argument
+// // // }
+// // // abcd(20);
 
-function abcd()
-{
-    return 12;    // Explicit return function
-}
-abcd();
+// // // function abcd()
+// // // {
+// // //     return 12;    // Explicit return function
+// // // }
+// // // abcd();
 
-let abcd = ()=> 12
-    {
-        // Fat arrow with implicit return
-    }
- var ans = abcd();
+// // // let abcd = ()=> 12
+// // //     {
+// // //         // Fat arrow with implicit return
+// // //     }
+// // //  var ans = abcd();
 
- // Rest parameter
+// // //  // Rest parameter
 
- function abcd(a,b,c,...rest)   // ... is rest
- {
-    console.log(a,b,c,rest);
- }
- abcd(1,2,3,4,5,6,7,8,9);
+// // //  function abcd(a,b,c,...rest)   // ... is rest
+// // //  {
+// // //     console.log(a,b,c,rest);
+// // //  }
+// // //  abcd(1,2,3,4,5,6,7,8,9);
+
+
+// // // let str = "HelloJavascript!";
+// // // console.log(str.includes("java"));
+
+// // // console.log(myFunc());
+// // // function myFunc()
+// // // {
+// // //     return "Hoisting";
+// // // }
+
+// // const greet = function()
+// // {
+// //     console.log('Hello!');
+// // }
+// // greet();
+// // greet = function()
+// // {
+// //     console.log('Hii');
+// // }
+
+// function outer()
+// {
+//     let count = 0
+//     return function inner()
+//     {
+//         count ++;
+//         console.log('count');
+//     };
+// }
+
+// const counter = outer();
+// counter();
+// counter();
