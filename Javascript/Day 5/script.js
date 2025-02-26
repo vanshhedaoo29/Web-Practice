9fa97dcbc43114eb2b8afe2c1fc5a5e7d90a4d31
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-02-26 10:20:14 +0530

    JS Day 5 and 6

---
+++ Javascript/Day 5/script.js
@@ -0,0 +1,37 @@
+// rest parameters
+
+// function abcd (a,b,c,...arr)
+// {
+//     console.log(a,b,c,arr);
+// }
+// abcd(1,2,3,4,5);             //   1 2 3 (2) [4, 5]
+
+// Hoisting
+
+// console.log(a);
+// a = 12; // Undefined
+// JS  main variables and functions unko js declaration wala part top pe move kar deta hai, this is called hoisting
+
+// IIFE : Immediately Invoked Function Expression
+
+// var ans = (function abcd()
+// {
+//     var a = 12;
+
+//     return
+//     {
+//         input: function(val)
+//         {
+//             a = val;
+//         }
+//         output: function()
+//         {
+//             console.log(a);
+//         }
+//     };
+// })();
+
+// ans.input("15");
+// ans.output();
+
+
---
+++ Javascript/Day 6/script.js
@@ -0,0 +1,51 @@
+
+// What is Array -----> Array is a data structure which can store multiple values all at once
+
+var a = [12,13,14];
+a
+
+// Why Arrays ----> Ek se jyada values store karne ke liye
+
+// Who uses Array ----> Developers
+
+// 
+
+
+
+
+
+
+
+var arr = [1,2,3,4];
+
+// Indexing
+/* arr [0] = 1 */
+/* arr [1] = 2 */
+/* arr [2] = 3 */
+/* arr [3] = 4 */
+
+var arr = [];      // Way 1
+
+var arr2 = new Array();  // Way 2
+
+
+
+
+// Array is heterogeneous in JS
+var arr = ["a", 12, function(){}, {}, [], NaN , undefined];
+
+// Objects
+
+/*What is Object --->  Collection of key value pairs*/
+
+var obj = {
+    name: "Vansh",             // obj.name
+    age: 19,
+    email: "vansh@gmail.com",
+};
+
+// How to create a Object
+
+var obj = {};
+
+var obj2 = new Object();
